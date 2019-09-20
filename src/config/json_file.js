7db9394a36884624fda1d45f6bb1484cf5fe9219
Christian Vermylen <christian.a.vermylen@gmail.com>
2019-09-19 20:22:12 -0400

    Add unit tests

--- src/config/json_file.js
+++ src/config/json_file.js
@@ -4,26 +4,15 @@ const parseConfig = (file_path) => {
     if (fs.existsSync(file_path)) {
         const content = fs.readFileSync(file_path, 'utf-8');
         const trace = JSON.parse(content.toString());
-        if (validate_config() (trace)) return trace;
+        let error_messages = [];
+        if (validate_config() (trace, error_messages)) return trace;
     } else {
         throw Error ('Config file missing:' + file_path);
     }
 };
 
-const validate_config = (trace_config) => {
-    if (trace_config.trace) return validate_trace (trace_config.trace);
-};
-
-const validate_trace = (trace) => {
-    if (!trace.request)
-        throw Error ('In config file, trace requires a request element');
-    if (!trace.request.output_format)
-        throw Error ('In config file, trace.request request an output_format');
-    validate_output_format (trace.request.output_format);
-
-    if (!trace.request.connector)
-        throw Error ('In config file, trce.request requires connector');
-    validate_connector (trace.request.connector);
+const validate_config = (trace_config, error_messages) => {
+    if (trace_config.trace) return validate_trace (trace_config.trace, error_messages);
 };
 
 const valid_parameters = [
@@ -32,12 +21,38 @@ const valid_parameters = [
     'server.datetime.now'
 ];
 
-const validate_output_format = (format) => {
-    const parameter_regex = /@{\w(\.\w)*}/g;
-    for (let param in format.match(parameter_regex)) {
+const validate_trace = (trace, error_messages) => {
+    validate_field(trace.request, error_messages, 'In config file, trace requires a request element');
+    validate_field(trace.request.output_format, error_messages, 'In config file, trace.request request an output_format');
+    validate_output_format (trace.request.output_format, valid_parameters);
+
+    validate_field(trace.request.connector, error_messages, 'In config file, trce.request requires connector');
+    validate_connector (trace.request.connector);
+};
+
+const validate_output_format = (format, accepted_parameters, list_of_errors) => {
+    let result = true;
+    const parameter_regex = /@{[a-z]+}/g;
+    // const parameter_regex = /@{\w(\.\w)*}/g;
+    for (let param in format.search(parameter_regex)) {
         const value = param.substr(2, param.length -1);
-        if(!valid_parameters.find(p => p === value)) {
-            throw Error('In config file, parameter ' + value + 'is not defined');
+        if(!accepted_parameters.find(p => p === value)) {
+            list_of_errors.push('In config file, parameter ' + value + 'is not defined');
+            result = false;
         }
     }
+    return result;
+};
+
+const validate_field = (field, list_of_errors, error_message) => {
+    if (!field) {
+        list_of_errors.push(error_message);
+        return false;
+    }
+    return true;
+};
+
+module.exports = {
+    validate_field,
+    validate_output_format
 };
---
+++ tests/config/json_file.test.js
@@ -0,0 +1,33 @@
+const json_file = require ('../../src/config/json_file');
+
+test ('should not throw an exception if the field exists', () => {
+    const o = {aField: 'aValue'};
+    let errors = [];
+    expect (json_file.validate_field(o.aField, errors,'Error message')).toBeTruthy();
+    expect(errors.length).toBe(0);
+});
+
+test ('should throw an error if the field is not present', () => {
+    const o = {aField: 'aValue'};
+    let errors = [];
+    expect(json_file.validate_field(o.bField, errors,'Error message')).toBeFalsy();
+    expect(errors.length).toBe(1);
+});
+
+test ('a valid parameter should be accepted in the format string', () => {
+    const valid_param = ['a.Param', 'b.param'];
+    let errors = [];
+    const output_format = 'alea @{a.Param} jacta @{b.Param} est';
+    expect(json_file.validate_output_format(output_format, valid_param, errors)).toBeTruthy();
+    expect(errors.length).toBe(0);
+});
+
+test ('an invalid parameter should be reported', () => {
+    const valid_param = ['a.param'];
+    let errors = [];
+    const output_format = '@{param} once upon a @{a.param} time @{b.param}';
+    expect (json_file.validate_output_format(output_format, valid_param, errors)).toBeFalsy();
+    expect(errors.length).toBe(2);
+    expect(errors[0]).toBe('param');
+    expect(errors[1]).toBe('b.param');
+});
